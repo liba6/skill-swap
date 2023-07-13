d3ed2a8200c321ef0ed3b0f3893473fc0de0c799
liba6 <lginsburg@gmail.com>
2023-07-13 23:43:09 +0200

    Add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    // Define your ESLint rules here
+  },
+};
